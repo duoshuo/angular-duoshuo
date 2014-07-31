87b3fcb7889802f0926c09b3ad87207287bf44de
Guo Yu <i@guoyu.me>
2014-07-31 16:35:08 +0800

    0.1: update license

--- src/duoshuo.js
+++ src/duoshuo.js
@@ -5,10 +5,16 @@
   if (!angular) throw new Error('angular.js required!');
   if (!duoshuo) throw new Error('duoshuo embed.js required!');
   if (!configs) throw new Error('duoshuoQuery object required');
+  if (!configs.short_name) throw new Error('duoshuo short_name required');
 
   angular.module('duoshuo', [])
     .factory('$duoshuo', function() {
       return new Duoshuo(configs);
     });
+
+  function Duoshuo() {
+    this.configs = configs;
+    this.short_name = configs.short_name;
+  }
 
 })(window.angular, window.DUOSHUO, window.duoshuoQuery);
