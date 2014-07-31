9b654351cfc602c7a421f6e5d4a7476f12c136a3
Guo Yu <i@guoyu.me>
2014-07-31 16:56:20 +0800

    0.1: update src

--- src/duoshuo.js
+++ src/duoshuo.js
@@ -1,11 +1,12 @@
-;(function(angular, duoshuo, configs) {
+;(function(angular, duoshuo, API, configs) {
 
   'use strict';
 
   if (!angular) throw new Error('angular.js required!');
   if (!duoshuo) throw new Error('duoshuo embed.js required!');
-  if (!configs) throw new Error('duoshuoQuery object required');
-  if (!configs.short_name) throw new Error('duoshuo short_name required');
+  if (!API) throw new Error('duoshuo embed.js must be unstable version!');
+  if (!configs) throw new Error('duoshuoQuery object required!');
+  if (!configs.short_name) throw new Error('duoshuo short_name required!');
 
   angular.module('duoshuo', [])
     .factory('$duoshuo', function() {
@@ -14,7 +15,29 @@
 
   function Duoshuo() {
     this.configs = configs;
-    this.short_name = configs.short_name;
+    this.events = ['reset', 'ready'];
   }
 
-})(window.angular, window.DUOSHUO, window.duoshuoQuery);
+  // lowlevel api set
+  Duoshuo.prototype.get = API.get;
+  Duoshuo.prototype.post = API.post;
+  Duoshuo.prototype.ajax = API.ajax;
+
+  // events
+  Duoshuo.prototype.on = function(eve, callback) {
+    if (this.events.indexOf(eve) === 0) return callback(new Error('event not found'));
+    var e = eve;
+    if (e === 'ready') e = 'reset';
+    return DUOSHUO.visitor.on(e, function(){
+      var self = this;
+      var data = this.data;
+      return callback(null, data, self);
+    });
+  };
+
+})(
+  window.angular, 
+  window.DUOSHUO, 
+  window.DUOSHUO.API,
+  window.duoshuoQuery
+);
